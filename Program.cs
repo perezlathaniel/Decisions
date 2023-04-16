51d5d91a70ec1e9ae2c4f2980de7e7673228d3ad
perezlathaniel <perezlathaniel@outlook.com>
2023-04-16 17:55:46 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lathaniel's Big Giveaway!");
+            Console.Write("Choose a door: 1, 2, 3: ");
+            string userValue = Console.ReadLine();
+
+            if (userValue == "1")
+            {
+                string message = "You won a new car!";
+                Console.WriteLine(message);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
